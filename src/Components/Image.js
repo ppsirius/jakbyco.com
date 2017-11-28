65084985fb5d20fb2478fe8e31fb5e26e1c3e7b8
Piotr Pupczyk <p.pupczyk@samsung.com>
2017-11-28 11:17:28 +0100

    change image animation

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { TweenMax } from 'gsap';
+import { animation } from '../Helpers/AnimationVariable';
 import '../css/Image.css';
 
 class Image extends Component {
   imageLoaded = () => {
-    window.dispatchEvent(new Event('animationImageComplete'));
+    this.animateOverlay();
+  }
+
+  animateOverlay = () => {
+    TweenMax.to(this.refs.imageLine, 1, {y: '100%', delay: .3, ease: animation.ease,
+      onComplete: () => TweenMax.to(this.refs.imageRect, 1, {x: '-100%', ease: animation.ease,
+        onComplete: () => window.dispatchEvent(new Event('animationImageComplete'))
+      })
+    })
   }
 
   render () {
@@ -11,11 +21,11 @@ class Image extends Component {
       <div className="image">
         <img
           src="/images/canvas-image.jpg"
-          alt="backgraund image"
+          alt="Waterfall in Norway"
           onLoad={this.imageLoaded}
         />
-        <div className="image-overlay image-line"></div>
-        <div className="image-overlay image-rect"></div>
+        <div className="image-overlay image-line" ref="imageLine"></div>
+        <div className="image-overlay image-rect" ref="imageRect"></div>
       </div>
     )
   }
