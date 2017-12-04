498366a45ee7573292f0de94e8327877096cd166
Piotrek <piotrek@Piotreks-iMac.local>
2017-12-04 21:58:41 +0100

    orientation lock info

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import MediaQuery from './Helpers/MediaQuery';
-import 'normalize.css/normalize.css';
-import './css/App.css';
+import React, { Component } from "react";
+import MediaQuery from "./Helpers/MediaQuery";
+import "normalize.css/normalize.css";
+import "./css/App.css";
 
-import ImageContainer from './Components/ImageContainer';
-import About from './Components/About';
-import LastProjects from './Components/LastProjects';
+import ImageContainer from "./Components/ImageContainer";
+import About from "./Components/About";
+import LastProjects from "./Components/LastProjects";
+import OrientationLock from "./Components/OrientationLock";
 
 class App extends Component {
-
   render() {
     return (
       <div className="app">
         <ImageContainer />
         <About />
-        {MediaQuery.isDesktop() &&
-          <LastProjects />
-        }
+        {MediaQuery.isDesktop() && <LastProjects />}
+        <OrientationLock />
       </div>
     );
   }
---
+++ src/Components/OrientationLock.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/OrientationLock.css';
+
+const OrientationLock = () => {
+  return (
+    <div className="orientation-lock">
+        <span className="lock-text">
+          Rotate device 😎
+        </span>
+    </div>
+  )
+}
+
+export default OrientationLock;
